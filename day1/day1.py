f23a41eace8dcff0db3d3605c9dbd3bf39d63f31
romarowski <romarowski@wisc.edu>
2023-12-13 18:46:50 -0500

    working day1 !

--- day1/day1.py
+++ day1/day1.py
@@ -10,18 +10,49 @@ def main():
     for line in data:
         coords.append(extract_coords(line))
 
+    with open('output.txt', 'w') as f:
+        # write out the list of coords each on a new line
+        for coord in coords:
+            f.write(str(coord) + '\n')
+
+
     print(sum(coords))
     
 def extract_coords(
         line : str
 ) -> int:
-    pattern = re.compile(r'\d')
-    match = pattern.findall(line)
-    if match is not None:
-        if len(match)>1:
-            return int(match[0] + match[-1])
-        elif len(match) == 1:
-            return int(match[0] + match[0])
+    options = ['one', 'two', 'three', 'four', 
+                'five', 'six', 'seven', 'eight', 
+                'nine']
+    pos_pattern = re.compile(r'one|two|three|four|five|six|seven|eight|nine|\d')   
+    neg_pattern = r''
+    for opt in options:
+        neg_pattern += opt[::-1] + '|'
+    neg_pattern += r'\d'
+    pos_match = pos_pattern.findall(line)
+    neg_pattern = re.compile(neg_pattern)
+    neg_match = neg_pattern.findall(line[::-1])
+    map_ = {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    }
+    if pos_match is not None:
+        n1 = pos_match[0]
+        if n1 in map_.keys():
+            n1 = map_[n1]
+    if neg_match is not None:
+        n2 = neg_match[0]
+        if n2[::-1] in map_.keys():
+            n2 = map_[n2[::-1]]
+    if n1 is not None and n2 is not None:
+        return int(n1 + n2)
     return 0
 
 
--- day1/day1_test.py
+++ day1/day1_test.py
@@ -1,8 +1,8 @@
 import unittest as ut
-from day1 import extract_coords, sum_coords
-
+from day1 import extract_coords
 
 class TestDay1(ut.TestCase):
+
 
     def test_extract_coords(self):
         self.assertEqual(
@@ -17,6 +17,42 @@ class TestDay1(ut.TestCase):
         self.assertEqual(
             extract_coords("treb7uchet"), 77
         )
+        self.assertEqual(
+            extract_coords("two1nine"), 29
+        )
+        self.assertEqual(
+            extract_coords("eightwothree"), 83
+        )
+        self.assertEqual(
+            extract_coords("abcone2threexyz"), 13
+        )
+        self.assertEqual(
+            extract_coords("xtwone3four"), 24
+        )
+        self.assertEqual(
+            extract_coords("4nineeightseven2"), 42
+        )
+        self.assertEqual(
+            extract_coords("zoneight234"), 14
+        )
+        self.assertEqual(
+            extract_coords("7pqrstsixteen"), 76
+        )
+        self.assertEqual(
+            extract_coords("one"), 11
+        )
+
+        self.assertEqual(
+            extract_coords("one6xzxmmjpone7ngdlvvhljmhztlbsgxthree"), 13
+        )
+
+
+
+
+
+
+
+
 
 
 
